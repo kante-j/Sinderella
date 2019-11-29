98e6e1683afc0609ad151e1b785f1f4c4f3739c2
MoHyeonMin <mhmrm@ajou.ac.kr>
2019-11-29 15:27:38 +0900

    click follower, following color change

--- app/src/main/java/com/jipjung/hucomin/sinderella/Fragments/FMymenu.java
+++ app/src/main/java/com/jipjung/hucomin/sinderella/Fragments/FMymenu.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -113,25 +114,38 @@ public class FMymenu extends Fragment {
             }
         });
 
+        //TODO: btn 색깔변화
+
         followerbtn = v.findViewById(R.id.mypage_follower);
+        followingbtn=v.findViewById(R.id.mypage_following);
+        Log.d("click",String.valueOf(followerbtn.isSelected()));
+        Log.d("click",String.valueOf(followingbtn.isSelected()));
+
         followerbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(followerbtn.isSelected()){
+                    Log.d("click","follower_false");
                     followerbtn.setSelected(false);
                 }else{
+                    Log.d("click","follower_true");
+                    followingbtn.setSelected(false);
                     followerbtn.setSelected(true);
                 }
             }
         });
 
-        followingbtn=v.findViewById(R.id.mypage_following);
+
         followingbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("click",String.valueOf(followingbtn.isSelected()));
                 if(followingbtn.isSelected()){
+                    Log.d("click","following_false");
                     followingbtn.setSelected(false);
                 }else{
+                    Log.d("click","following_true");
+                    followerbtn.setSelected(false);
                     followingbtn.setSelected(true);
                 }
             }
